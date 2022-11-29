788445874e9fcb56fd0f4d108bf081cd25f64b0f
alicankasap <eposta@adresiniz.comalicankasap96@gmail.com>
2022-11-29 19:54:26 +0300

    first commit

---
+++ Backend/Business/Constants/Messages.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 15 ürün olabilir";
+        public static string ProductNameAlreadExist = "Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldı";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserRegistered = "Kullanıcı başarıyla oluşturuldu";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Hatalı şifre tuşladınız";
+        public static string SuccessfulLogin = "Başarıyla giriş yapıldı";
+        public static string UserAlreadyExists = "Kullanıcı zaten mevcut";
+        public static string AccessTokenCreated = "Erişim token'ı oluşturuldu";
+    }
+}
---
+++ Backend/ConsoleUI/Program.cs
@@ -0,0 +1,47 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ProductManager productManager = new ProductManager(new EfProductDal());
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            //productManager.Add(new Product { CategoryId = 1, ProductName = "T", UnitPrice = 1000, UnitsInStock = 1 });
+
+            //foreach (var product in productManager.GetAllByCategoryId(2))
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            //foreach (var category in categoryManager.GetAll())
+            //{
+            //    Console.WriteLine(category.CategoryName);
+            //}
+
+            //foreach (var product in productManager.GetProductDetails())
+            //{
+            //    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            //}
+
+            //Console.WriteLine(categoryManager.GetById(1).CategoryName);
+
+            //var result = productManager.GetAll();
+
+            //if (result.Success)
+            //{
+            //    foreach (var product in productManager.GetAll().Data)
+            //    {
+            //        Console.WriteLine(product.ProductName);
+            //    }
+            //}
+            //Console.WriteLine(result.Message);
+        }
+    }
+}
